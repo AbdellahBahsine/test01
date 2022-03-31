fbf1407615b74b561a6b74e02fd6f6fbd3225b0b
paylo <abdellah.bahsine@hotmail.com>
2022-03-31 10:38:58 +0100

    structured the app, and installed the necessary libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* <Route path='*' element={ <NotFoundPage /> } /> */}
+        {/* <Route path='/dashboard/*' element={!isLoggedIn ? <Navigate to="/" /> : <DashboardPage />} /> */}
+        {/* <Route exact path='/' element={isLoggedIn ? <Navigate to="/dashboard" /> : <LoginPage />} /> */}
+
+      </Routes>
     </div>
   );
 }
