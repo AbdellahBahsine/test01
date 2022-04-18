33511964cc37037a51553ec00528cb0a6dc01443
paylo <abdellah.bahsine@hotmail.com>
2022-04-18 14:15:03 +0100

    changed hero and header animations...

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -5,9 +5,9 @@ const HeroComponent = () => {
     return (
         <section className="hero-component">
             <div className="hero__image"></div>
-            <p id="textOne" >Hello! Welcome to</p>
-            <h1 id="textTwo" ><span>L</span>e <span>T</span>raveler <span>G</span>uide</h1>
-            <p id="textThree" >A blog that's going to introduce you to amazing places in the world.</p>
+            <p className="textOne" >Hello! Welcome to</p>
+            <h1 className="textTwo" ><span>L</span>e <span>T</span>raveler <span>G</span>uide</h1>
+            <p className="textThree" >A blog that's going to introduce you to amazing places in the world.</p>
         </section>
     )
 }
